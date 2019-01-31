9841ce987733bb341e9523171fd8dad3861934eb
Woodsandfields <k.gantin.kg@gmail.com>
2019-01-31 08:20:09 +0100

    fix bash program to select a 2d county

--- data/2019/2019-01-22/my_bash_script_to_subset_data.sh
+++ data/2019/2019-01-22/my_bash_script_to_subset_data.sh
@@ -3,8 +3,13 @@
 
 grep -i 'GA.*Jasper' pretrial_population.csv > my_data_subset.csv
 
+echo "This program just created a dataset called my_data_subset.csv in your folder with all pretrial_population data for Jasper County, GA."
 
-awk -F ',' '{if (($1>2000)&&($2=="AL")&&($7=="Total")&&($8<9000)&&) print $3;}' pretrial_population.csv | uniq > my_AL_preselec.csv
+echo "Now, let's find a county in Alabama with similar characteristics to Jasper, GA and merge the data to have a dataset with two counties for pretrial population".
+
+echo "Currently, the selection criteria are: total population under 9000 after 2000 and located in Alabama."
+
+awk -F ',' '{if (($1>2000)&&($2=="AL")&&($7=="Total")&&($8<9000)) print $3;}' pretrial_population.csv | uniq > my_AL_preselec.csv
 
 cat my_AL_preselec.csv
 
@@ -14,14 +19,15 @@ read county
 
 grep AL pretrial_population.csv | grep $county
 
-echo "Do you confirm you want to select this one? Y/N"
+echo "Do you confirm you want to select this one? y/n"
 
 read confirm
 
 echo $confirm
 
-if [$confirm -eq "y"]
+if [ $confirm == "y" ]
 then
-echo "I got you"
-
+echo "Let's then merge the data with "
+else 
+echo "Select a county again"
 fi
